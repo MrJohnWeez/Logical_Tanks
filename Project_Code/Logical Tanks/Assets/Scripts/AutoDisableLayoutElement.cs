bb2dbf07df6fe639cc48661a18b214b2c5c3d2e4
MrJohnWeez <johnwakeboarding@gmail.com>
2020-10-01 19:41:19 -0400

    Added UI extensions and started making poc for node/block UI

---
+++ Project_Code/Logical Tanks/Assets/Scripts/AutoDisableLayoutElement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LayoutElement))]
+public class AutoDisableLayoutElement : MonoBehaviour
+{
+    LayoutElement test = null;
+
+    private void Start()
+    {
+        test = GetComponent<LayoutElement>();
+    }
+}
---
+++ Project_Code/Logical Tanks/Assets/Scripts/ForceHeight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LayoutElement))]
+public class ForceHeight : MonoBehaviour
+{
+    LayoutElement test = null;
+    float test2 = 0;
+
+    private void Start()
+    {
+        test = GetComponent<LayoutElement>();
+    }
+
+    private void Update() {
+        int test3 = gameObject.transform.childCount;
+        int ending = 38 + 80 * test3 + 38;
+        test.preferredHeight = ending;
+    }
+}
