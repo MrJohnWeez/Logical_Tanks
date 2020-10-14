10688f22d99f1dbe4a23f2b775775193795ee561
MrJohnWeez <johnwakeboarding@gmail.com>
2020-10-13 22:18:49 -0400

    Got basic camera movement working

--- Project_Code/Logical Tanks/Assets/Scripts/CameraManager.cs
+++ Project_Code/Logical Tanks/Assets/Scripts/CameraManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    // Figure out how to move the camera properly
+    private float sensitivity = 0.024f;
     private const float WALL_OFFSET = 1.5f;
 
     [SerializeField] private Camera _mapCamera = null;
+    [SerializeField] private PaneControls _paneControls = null;
 
     private Transform _mapCameraTransform = null;
     private Transform[] _cameraWalls = new Transform[4];
     private Vector2 _xLimits = new Vector3();
     private Vector2 _zLimits = new Vector3();
+    private Vector2 _deltaBuffer = new Vector2();
 
     private void Awake()
     {
@@ -34,14 +38,28 @@ public class CameraManager : MonoBehaviour
             }
         }
         else { Debug.LogError("Invalid number of CameraWall(s) in scene"); }
+        _paneControls.OnDragEvent += MoveCamera;
+    }
+
+    private void OnDestroy()
+    {
+        _paneControls.OnDragEvent -= MoveCamera;
+    }
+
+    private void MoveCamera(Vector2 delta)
+    {
+        _deltaBuffer += delta * sensitivity;
     }
 
     private void LateUpdate()
     {
         Vector3 camPos = _mapCameraTransform.position;
+        camPos.x -= _deltaBuffer.x;
+        camPos.z -= _deltaBuffer.y;
         float offset = _mapCamera.orthographicSize + WALL_OFFSET;
         camPos.x = Mathf.Clamp(camPos.x, _xLimits.y + offset, _xLimits.x - offset);
         camPos.z = Mathf.Clamp(camPos.z, _zLimits.y + offset, _zLimits.x - offset);
         _mapCameraTransform.position = camPos;
+        _deltaBuffer = Vector2.zero;
     }
 }
---
+++ Project_Code/Logical Tanks/Assets/Scripts/UI/GameControl/PaneControls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class PaneControls : MonoBehaviour, IDragHandler
+{
+    public Action<Vector2> OnDragEvent;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        OnDragEvent?.Invoke(eventData.delta);
+    }
+}
